40f8920fc87413b8da609368f0df12f618fb3d40
Rodrigo <rodrigomartins_santos1804@hotmail.com>
2023-07-05 22:59:57 -0300

    feat: component ModalButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Home } from './components/Home/Home';
+import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login';
 import { PetDetails } from './pages/PetDetails';
 import { PestList } from './pages/PetsList';
--- src/pages/PetDetails/styles.ts
+++ src/pages/PetDetails/styles.ts
@@ -6,32 +6,7 @@ align-items: center;
 justify-content: center;
 `;
 
-export const ModalButton = styled.div`
-position: absolute;
-top: 0;
-left: 0;
-width: 6rem;
-height: 100vh;
-background: #F15156;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: space-between;
-padding: 32px 0 ;
 
-button {
-  width: 3rem;
-  height: 3rem;
-  border-radius: 0.9375rem;
-  background: #F4D35E;
-  border: none;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-}
-`;
 
 export const DetailPetContainer = styled.div`
   max-width: 35rem;
